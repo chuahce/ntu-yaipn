2541ec64adff6d2acf86459e9237e976f38bf67c
chuahce <choongeng.chuah@gmail.com>
2025-02-14 01:29:27 +0800

    first-commit day01

---
+++ java-programming-part-1/for-first-time-programmers/introduction-to-java-programming/Hello.java
@@ -0,0 +1,10 @@
+/*
+ * First Java program, which says Hello
+ */
+public class Hello { // Save as "Hello.java"
+
+  public static void main(String[] args){ // Program entry point
+    System.out.println("Hello, World!"); // Print text message
+  }
+}
+

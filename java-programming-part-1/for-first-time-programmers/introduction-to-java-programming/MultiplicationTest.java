a4efb143c83cbb02bf10a4ff20101bb93b6c64b2
chuahce <choongeng.chuah@gmail.com>
2025-02-16 16:47:36 +0800

    day03

---
+++ java-programming-part-1/for-first-time-programmers/introduction-to-java-programming/MultiplicationTest.java
@@ -0,0 +1,15 @@
+/*
+ * Print the sum of 31 times of number1 and 17 times of number2
+ */
+
+public class MultiplicationTest {
+
+  public static void main(String[] Args){
+    int number1 = 98; // Declare an int variable number1 and initialize it to 98
+    int number2 = 5; // Declare an int variable number2 and initialize it to 5
+
+    int sum = (31 * number1) + (17 * number2);
+
+    System.out.println("The sum of 31 times of number1 and 17 times of number2 is " + sum);
+  }
+}
